84a664cf12ba6807b3aa703d7337d6e51408c7e4
SplitKrosGT <albertsadofficial@gmail.com>
2022-01-12 23:10:42 +0500

    save

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import './styles/App.css';
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+
+function App() {
+  const [posts, setPosts] = useState ([
+    {id: 1, title: 'JavaScript 1', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'},
+  ])
+  const [posts2, setPosts2] = useState ([
+    {id: 1, title: 'Python 1', body: 'Description'},
+    {id: 2, title: 'Python 2', body: 'Description'},
+    {id: 3, title: 'Python 3', body: 'Description'},
+  ])
+
+  return (
+    <div className="App">
+      <PostList posts={posts} title={'Посты про JS'}/>
+      <PostList posts={posts2} title={'Посты про Python'}/>
+    </div>
+  );
+}
+
+export default App;
